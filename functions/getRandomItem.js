15411f81321156eeabdac27db7727e6d91c08abc
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2018-05-22 18:14:16 -0300

    Add <getRandomItem.js>

---
+++ functions/getRandomItem.js
@@ -0,0 +1,5 @@
+const getRandomItem = arr => arr[Math.floor(Math.random() * arr.length)];
+
+// const items = [12, false, 548 , 'a' , 2 , 5478 , 'foo' , 88, , 'Doe' , 0000 , 119];
+// console.log(getRandomItem(items));
+
