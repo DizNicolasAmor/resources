bacdd4f91b0f4baf5145c1075093024cef72845a
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2018-05-23 10:09:03 -0300

    Add <shuffleArray.js>

---
+++ functions/shuffleArray.js
@@ -0,0 +1,6 @@
+const shuffleArray = arr => arr.sort(()=> Math.random()-0.5);
+
+// const numbers = [1, 2, 3, 4, 5, 458 , 120 , -215 , 400 , 12000, -85000];
+// console.log(shuffleArray(numbers));
+
+
