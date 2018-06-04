570244b1cd258430136cbcc810e6948927e13bce
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2018-06-04 10:10:00 -0300

    Add <isNumber.js>

---
+++ functions/isNumber.js
@@ -0,0 +1,5 @@
+const isNumber = n => !isNaN(parseFloat(n)) && isFinite(n);
+
+// const inputs = [123, '123', 123.456, -123.00, false, true, undefined];
+// console.log(inputs.map(i=>isNumber(i)));
+
