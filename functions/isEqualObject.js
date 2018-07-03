edbb0f8bf67d74194e8f5e006beabf0d0935582e
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2018-07-03 10:24:20 -0300

    Add <isEqualObject.js>

---
+++ functions/isEqualObject.js
@@ -0,0 +1,18 @@
+function isEqualObject(obj1, obj2) {
+  if (obj1 === obj2)
+    return true;
+
+  const prototypesAreEqual = isEqualObject(
+        Object.getPrototypeOf(obj1), Object.getPrototypeOf(obj2)
+      );
+
+  const obj1Props = Object.getOwnPropertyNames(obj1)
+  const obj2Props = Object.getOwnPropertyNames(obj2)
+
+  return (
+    obj1Props.length === obj2Props.length &&
+    prototypesAreEqual &&
+    obj1Props.every(prop => isEqualObject(obj1[prop], obj2[prop]))
+  )
+}
+
