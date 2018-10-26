3ffd727aef4010aefc225c4593e7e2e75c36e233
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2018-10-26 18:27:58 -0300

    add alignItems(value)

--- styled-components/style-utils.js
+++ styled-components/style-utils.js
@@ -1,9 +1,14 @@
 // style-utils.js
 
-export const truncate = (width) => `
+export const truncate = width => `
     width: ${width};
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
 `;
 
+export const alignItems = value => `
+	-webkit-align-items: ${value};
+	-ms-align-items: ${value};
+	align-items: ${value};
+`;
