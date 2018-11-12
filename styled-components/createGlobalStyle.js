54e5407cab14769a5ac1ac56230ee728c59ea91f
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2018-11-12 09:53:21 -0300

    Add <createGlobalStyle.js>

---
+++ styled-components/createGlobalStyle.js
@@ -0,0 +1,11 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+	body {
+		margin: 0;
+		padding: 0;
+	}
+`;
+
+// render in your component -->	<GlobalStyle />
+
