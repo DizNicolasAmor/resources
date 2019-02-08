800fcd2ec2944ea0da49eb9c1e95c2adafb305d6
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2019-02-08 09:59:21 -0300

    Add function 'last'

--- handlebars/helpers.js
+++ handlebars/helpers.js
@@ -1,2 +1,3 @@
 {{eval "Handlebars.registerHelper('toUpperCase', function(str) { try{ return str.toUpperCase(); } catch(err) { return err; } });" }}
 
+{{eval "Handlebars.registerHelper('last', function(array) { return array[array.length - 1]; });" }}
