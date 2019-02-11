94e07f82de63e0f1eb6ee5abe7452262303f8496
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2019-02-11 10:04:41 -0300

    Add first(array)

--- handlebars/helpers.js
+++ handlebars/helpers.js
@@ -1,3 +1,5 @@
 {{eval "Handlebars.registerHelper('toUpperCase', function(str) { try{ return str.toUpperCase(); } catch(err) { return err; } });" }}
 
 {{eval "Handlebars.registerHelper('last', function(array) { return array[array.length - 1]; });" }}
+
+{{eval "Handlebars.registerHelper('first', function(array) { return array[0]; });" }}
