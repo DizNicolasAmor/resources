62161f0055da85cdcd1a32d00b89a745a78eeb59
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2019-03-04 08:06:38 -0300

    Add PasswordRevealer.jsx

---
+++ react/PasswordRevealer.jsx
@@ -0,0 +1,17 @@
+const PasswordRevealer = ({ value }) => {
+  const [shown, setShown] = React.useState(false);
+
+  return (
+    <div>
+      <input
+        type={shown ? "text" : "password"}
+        value={value}
+        onChange={() => {}}
+      />
+      <button onClick={() => setShown(!shown)}>Show/Hide</button>
+    </div>
+  );
+}
+
+export default PasswordRevealer;
+
