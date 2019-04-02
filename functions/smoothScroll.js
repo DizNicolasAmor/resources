72b79a8a2418e79400d5f175f263f691a84fbba0
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2019-04-02 12:08:56 -0300

    Update smoothScroll.js

--- functions/smoothScroll.js
+++ functions/smoothScroll.js
@@ -1,1 +1,5 @@
+/* from current position to plus or minus px */
 $('html, body').animate({ scrollTop: '+=' + scrollDistance }, scrollTime, 'linear');
+
+/* jump to specific position in window.innerHeight */
+$('html, body').animate({ scrollTop: scrollDistance }, scrollTime, 'linear');
