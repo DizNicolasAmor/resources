b029c10e046c28adaa22bcdc1582a85a68d6c127
Nico Diz <diz.nicolasamor@gmail.com>
2021-05-15 11:16:04 -0300

    Create a11y/constants.js

---
+++ a11y/constants.js
@@ -0,0 +1,14 @@
+export const KEYBOARD_KEYS = {
+    ARROW_DOWN: 'ArrowDown',
+    ARROW_LEFT: 'ArrowLeft',
+    ARROW_RIGHT: 'ArrowRight',
+    ARROW_UP: 'ArrowUp',
+    END: 'End',
+    ENTER: 'Enter',
+    ESC: 'Esc',
+    ESCAPE: 'Escape',
+    HOME: 'Home',
+    SPACE: 'Space',
+    SPACE_IE: ' ',
+    TAB: 'Tab',
+};
