2b0f9eb342906882998b465014a81cb396d8bf57
Nicolas Diz <diz.nicolasamor@gmail.com>
2023-08-16 17:14:19 -0300

    MASTER: update isNumber.js

--- functions/isNumber.js
+++ functions/isNumber.js
@@ -1,5 +1,9 @@
 const isNumber = n => !isNaN(parseFloat(n)) && isFinite(n);
 
-// const inputs = [123, '123', 123.456, -123.00, false, true, undefined];
-// console.log(inputs.map(i=>isNumber(i)));
+const inputs = [123, '123', 123.456, -123.00, false, true, undefined];
 
+console.log('SIMPLE TESTS:');
+
+inputs.forEach(i => {
+    console.log(`${isNumber(i)}: ${i} is number`);
+});
