adfc758dff6f1095dd3b8f87eb10664beb8becdf
Nicolas Diz <diz.nicolasamor@gmail.com>
2023-09-25 09:41:23 -0300

    MASTER: create functions/scrollToTop.ts

--- functions/scrollToTop.js
+++ functions/scrollToTop.js
@@ -5,4 +5,3 @@ const scrollToTop = () => {
     window.scrollTo(0, c - c / 8);
   }
 };
-
---
+++ functions/scrollToTop.ts
@@ -0,0 +1,8 @@
+export const scrollToTop = (): void => {
+  const c: number = document.documentElement.scrollTop || document.body.scrollTop;
+
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, c - c / 8);
+  }
+};
