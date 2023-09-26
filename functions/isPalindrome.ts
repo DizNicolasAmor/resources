f3066e6ac2b5dc47d30e145c45dc448c0a3d29aa
Nicolas Diz <diz.nicolasamor@gmail.com>
2023-09-26 10:06:53 -0300

    MASTER: create functions/isPalindrome.ts

---
+++ functions/isPalindrome.ts
@@ -0,0 +1,11 @@
+export const isPalindrome = (str: string): boolean => {
+  const length = str.length;
+  let i = 0;
+
+  while (i <= length - 1 - i) {
+    if(str[i] !== str[length - 1 - i])
+      return false;
+    i += 1;
+  }
+  return true;
+};
