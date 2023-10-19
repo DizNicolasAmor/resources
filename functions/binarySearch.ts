f8842f47645627f23a447c20139a0b91bbd7ada8
Nicolas Diz <diz.nicolasamor@gmail.com>
2023-10-19 14:51:29 -0300

    MASTER: functions/binarySearch.ts

---
+++ functions/binarySearch.ts
@@ -0,0 +1,13 @@
+export const binarySearch = (list: number[], value: number): number => {
+	let start = 0;
+	let stop = list.length - 1;
+	let middle = Math.floor((start + stop) / 2);
+
+	while (list[middle] !== value && start < stop) {
+		if (value < list[middle])  stop = middle - 1;
+		else  start = middle + 1;
+		middle = Math.floor((start + stop) / 2);
+	}
+
+	return list[middle] === value ? middle : -1;
+};
