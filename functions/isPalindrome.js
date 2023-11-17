7b0b09fa7e28e43bf4fbcae2fd75aabf0ae7334e
Nicolas Diz <diz.nicolasamor@gmail.com>
2023-11-17 13:40:14 -0300

    MASTER: update functions/isPalindrome.js

--- functions/isPalindrome.js
+++ functions/isPalindrome.js
@@ -1,14 +1,15 @@
 const isPalindrome = str => {
-    let i = 0;
-    const length = str.length;
-    while (i <= length - 1 - i) {
-      if(str[i] !== str[length - 1 - i])
-        return false;
-      i += 1;
-    }
-    return true;
+  const length = str.length;
+  let i = 0;
+
+  while (i <= length - 1 - i) {
+    if(str[i] !== str[length - 1 - i])
+      return false;
+    i += 1;
+  }
+
+  return true;
 };
 
 // console.log(isPalindrome('abbcbba'));
 // console.log(isPalindrome('abcqwe'));
-
