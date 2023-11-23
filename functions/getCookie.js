55afad84cb459b823f578a58817a69011d0f2a56
Nicolas Diz <diz.nicolasamor@gmail.com>
2023-11-23 15:18:56 -0300

    MASTER: create functions/getCookie.js

---
+++ functions/getCookie.js
@@ -0,0 +1,11 @@
+const getCookie = (name) => {
+  const nameLenPlus = name.length + 1;
+
+  return (
+    document.cookie
+      .split(';')
+      .map(c => c.trim())
+      .filter(cookie => cookie.substring(0, nameLenPlus) === `${name}=`)
+      .map(cookie => decodeURIComponent(cookie.substring(nameLenPlus)))[0] || ''
+    );
+};
