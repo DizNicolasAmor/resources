48e7bcce06aa8ac85d17a418d1f43f7d465749b0
Nicolas Diz <diz.nicolasamor@gmail.com>
2023-11-24 11:32:39 -0300

    MASTER: update functions/getCookie.ts

--- functions/getCookie.ts
+++ functions/getCookie.ts
@@ -1,10 +1,10 @@
 export const getCookie = (name: string): string => {
-    const nameLenPlus = name.length + 1;
-    return (
-        document.cookie
-            .split(';')
-            .map(c => c.trim())
-            .filter(cookie => cookie.substring(0, nameLenPlus) === `${name}=`)
-            .map(cookie => decodeURIComponent(cookie.substring(nameLenPlus)))[0] || ''
-    );
+  const nameLenPlus = name.length + 1;
+  return (
+    document.cookie
+      .split(';')
+      .map(c => c.trim())
+      .filter(cookie => cookie.substring(0, nameLenPlus) === `${name}=`)
+      .map(cookie => decodeURIComponent(cookie.substring(nameLenPlus)))[0] || ''
+  );
 };
