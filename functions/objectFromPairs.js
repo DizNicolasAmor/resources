184499e5e688e700a21691c2572c252c8e5f1cf1
Nicolas Diz <diz.nicolasamor@gmail.com>
2023-11-30 10:21:12 -0300

    MASTER: update functions/objectFromPairs.js

--- functions/objectFromPairs.js
+++ functions/objectFromPairs.js
@@ -1,4 +1,8 @@
+/**
+ * Creates an object from an array of pairs
+ * @param { Array<Array<string, any>> } arr - The array of pairs
+ * @example
+ * // returns { a: 1, b: 2 }
+ * objectFromPairs([['a', 1], ['b', 2]])
+ */
 const objectFromPairs = arr => arr.reduce((a, [key, val]) => ((a[key] = val), a), {});
-
-// objectFromPairs([['a', 1], ['b', 2]]); // {a: 1, b: 2}
-
